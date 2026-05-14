cf2eeed36b037b818852eb9952ca2d783ed1e0bb
IgorVarani <igor.duo@aluno.senai.br>
2026-05-14 10:59:41 -0300

    :construction: Continuando a tela Home

--- Front/royal-games/src/pages/home/index.tsx
+++ Front/royal-games/src/pages/home/index.tsx
@@ -10,8 +10,8 @@ const Home = () => {
             <Header/>
             <ToastContainer/>
             <main id={styles.main}>
-                <section>
-                    <div>
+                <section id={styles.banner}>
+                    <div id={styles.banner_esquerda}>
                         <h1>Conheça nossos jogos!</h1>
                         <p>
                             Navegue por títulos de todas as gerações, descubra plataformas, 
@@ -19,25 +19,25 @@ const Home = () => {
                             Seu próximo jogo favorito começa aqui.
                         </p>
                     </div>
-                    <div>
-                        <img src="" alt="Imagem de uma mulher em um estilo cyberpunk." />
+                    <div id={styles.banner_direita}>
+                        <img src="/imgs/home.svg" alt="Imagem de uma mulher em um estilo cyberpunk." />
                     </div>
                 </section>
 
-                <section>
+                <section id={styles.catalogo}>
                     <h2>Catálogo de Jogos</h2>
                     <hr id={styles.linha_h2}></hr>
                 </section>
 
-                <section>
-                    <h3>Jogos online podem afetar o comportamento humano?</h3>
-                    <hr id={styles.linha_h3}></hr>
-                    <div id={styles.container}>
-                        <img src="" alt="Imagem do jogo League of Legends." />
-                        <img src="" alt="Imagem do jogo Counter-Strike." />
+                <section id={styles.comportamento}>
+                        <h3>Jogos online podem afetar o comportamento humano?</h3>
+                        <hr id={styles.linha_h3}></hr>
+                    <div id={styles.comportamento_container}>
+                        <img src="/imgs/lol.svg" alt="Imagem do jogo League of Legends." />
+                        <img src="/imgs/cs.svg" alt="Imagem do jogo Counter-Strike." />
                     </div>
-                    <p>Estudos indicam que jogos podem alterar o comportamento humano...</p>
-                    <p>Principalmente quando o time resolve testar sua paciência em plena partida ranqueada.</p>
+                    <p className={styles.comportamento_texto}>Estudos indicam que jogos podem alterar o comportamento humano...</p>
+                    <p className={styles.comportamento_texto}>Principalmente quando o time resolve testar sua paciência em plena partida ranqueada.</p>
                 </section>
             </main>
             <Footer/>
