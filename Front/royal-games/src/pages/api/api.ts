53833076288af4751f4dbd3449f555d5394a8c5d
IgorVarani <igor.duo@aluno.senai.br>
2026-05-16 20:29:10 -0300

    :sparkles: feat: Integrando a API (Back-End) na tela Login

---
+++ Front/royal-games/src/pages/api/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import secureLocalStorage from "react-secure-storage";
+
+const apiLocal = "https://localhost:7044/api/"; //? Criar um endereco da API local. Ex: http://localhost:3000/api
+
+const apiRemota = ""; //? Criar um endereco da API remota. Ex: https://api.minhaapp.com
+
+//? Criar um endereco da API dentro do axios.
+export const api = axios.create({
+    baseURL: apiLocal
+})
+
+//? É um interceptor do Axios, ele intercepta toda requisição antes de ser enviada.
+api.interceptors.request.use((config) => {
+    const token = secureLocalStorage.getItem("Token");
+
+    if(token)
+    {
+        config.headers.Authorization = "Bearer " + token;
+    }
+
+    return config;
+});
--- Front/royal-games/src/pages/login/index.tsx
+++ Front/royal-games/src/pages/login/index.tsx
@@ -1,8 +1,38 @@
 import { Fragment } from "react/jsx-runtime";
 import styles from "./login.module.css"
 import { ToastContainer } from "react-toastify/unstyled";
+import { login } from "../api/authService";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const Login = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [senha, setSenha] = useState<string>("");
+
+    const router = useRouter();
+    const notificao = (msg: string ) => toast.success(msg);
+    const erro = (msg: string) => toast.error(msg);
+
+    async function autenticar(e: React.FormEvent<HTMLFormElement>)
+    {
+        e.preventDefault();
+        try
+        {
+            await login(email, senha);
+            notificao("Login bem sucedido!");
+
+            setTimeout(() => {
+                router.push("/home");
+            }, 2000);
+        }
+        catch(error: any)
+        {
+            erro(error.message);
+        }
+    }
+
     return (
         <Fragment>
             <ToastContainer/>
@@ -11,15 +41,17 @@ const Login = () => {
                 <section id={styles.section}>
                     <div id={styles.campo_login}>
                         <img src="/imgs/logo.svg" alt="Logo do site."/>
-                        <form id={styles.formulario}>
+                        <form id={styles.formulario} onSubmit={autenticar}>
                             <div className={styles.campo_form}>
                                 <label htmlFor="email">Email</label>
-                                <input type="text" name="email" placeholder="email@exemplo.com"/>
+                                <input type="text" name="email" placeholder="email@exemplo.com"
+                                value={email} onChange={(e) => setEmail(e.target.value)}/>
                             </div>
                             
                             <div className={styles.campo_form}>
                                 <label htmlFor="senha">Senha</label>
-                                <input type="password" name="senha" placeholder="********"/>
+                                <input type="password" name="senha" placeholder="********"
+                                value={senha} onChange={(e) => setSenha(e.target.value)}/>
                             </div>
                             
                             <div className={styles.campo_button}>    
