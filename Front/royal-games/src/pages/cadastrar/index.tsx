86c4a7151a8014905a6d46ad387b41ccab267f3c
IgorVarani <igor.duo@aluno.senai.br>
2026-05-16 00:39:36 -0300

    :sparkles: feat: Todas as pages finalizadas

---
+++ Front/royal-games/src/pages/cadastrar/index.tsx
@@ -0,0 +1,84 @@
+import { Fragment } from "react/jsx-runtime";
+import styles from "./cadastrar.module.css";
+import { ToastContainer } from "react-toastify";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import Lista from "@/components/jogo-lista/jogo-lista";
+
+const Cadastrar = () => {
+    return (
+        <Fragment>
+            <Header />
+            <ToastContainer />
+            <main id={styles.main}>
+                <section id={styles.section}>
+                    <div id={styles.campo_cadastro}>
+                        <h1>Cadastrar Novo Jogo</h1>
+                        <hr />
+                        <div id={styles.formulario}>
+                            <div id={styles.esquerda}>
+                                <div className={styles.campo}>
+                                    <label>Nome</label>
+                                    <input type="text" />
+                                </div>
+
+                                <div className={styles.linha}>
+
+                                    <div className={styles.campo}>
+                                        <label>Valor</label>
+                                        <input type="text" />
+                                    </div>
+
+                                    <div className={styles.campo}>
+                                        <label>Gênero</label>
+                                        <select>
+                                            <option></option>
+                                        </select>
+                                    </div>
+
+                                    <div className={styles.campo}>
+                                        <label>Classificação Indicativa</label>
+                                        <select>
+                                            <option></option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className={styles.linha}>
+
+                                    <div className={styles.campo}>
+                                        <label>Plataforma</label>
+                                        <select>
+                                            <option></option>
+                                        </select>
+                                    </div>
+
+                                    <div className={styles.campo}>
+                                        <label>Imagem</label>
+                                        <input type="file" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div id={styles.direita}>
+                                <label>Descrição</label>
+                                <textarea></textarea>
+                            </div>
+
+                        </div>
+                        <button id={styles.botao}>Cadastrar</button>
+                    </div>
+                </section>
+
+                <section id={styles.lista}>
+                    <h2>Lista de Jogos</h2>
+                    <hr id={styles.linha_h2}></hr>
+                    <Lista/>
+                </section>
+            </main>
+            <Footer />
+        </Fragment>
+    );
+}
+
+export default Cadastrar;
